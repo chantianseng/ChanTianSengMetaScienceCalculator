45632c584e0459b7416cbe140a392d57d299b6a4
chantianseng <sysysysysy90@gmail.com>
2026-09-06 15:51:01 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const CACHE="xuanheng-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","index.html","manifest.webmanifest","icon.svg"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
+const CACHE="chantianseng-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","index.html","manifest.webmanifest","chantiansengicon.jpg"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
